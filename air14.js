b9d1f9ffcbf97b0580f6eaf6aef5c63ac1b39464
Semih Gokol <gokol.semih@gmail.com>
2022-12-14 13:03:59 +0100

    job is done bro

--- air14.js
+++ air14.js
@@ -1,59 +1,6 @@
-/* --- AIR : OK ---
-
-Créez un programme qui célèbre votre victoire.
-
-
-Exemples d’utilisation :
-$> python exo.py
-J’ai terminé l’Épreuve de l’Air et c’était [].
-$>
-
-Note : [] est à remplacer par un adjectif de votre choix (facile ?)
-
-*/
-
-//Initialisation des variables
-let args = process.argv.slice(2)
-let arg1 = process.argv[2]
-let arg2 = process.argv[3]
-let res = ""
-const nbRegex = /^-?[0-9]\d*(\.\d+)?$/
-
-//f() utilisées
-//Number test
-function isNotNumber(args){
-    let counter = 0
-    for (let i = 0; i < args.length; i++) {
-        if (args[i].match(nbRegex)) {
-            counter ++
-        } else {
-            
-            res = true
-        }        
-    }
-    return counter
-}
-
-
-//Main f()
-function main(){
-
-}
-
-//Gestion d'erreurs
-if (arg1 == undefined || arg2 == undefined) {
-    console.log("Merci d'entrer 2 arguments minimum valables");
+function done() {
+    console.log("Encore une masterclass du joueur Franco-Turc, maintenant c'est tout droit");
     return
 }
 
-//Inverse de ma f() isNumber() de l'exo précedent 
-if (isNotNumber(args) > 0) {
-    console.log("Ce script ne peut prendre que des lettres/mots en arguments");
-    return
-}
-
-//Traitement
-let result = 
-
-//Affichage résultat
-console.log(result);
+done()
